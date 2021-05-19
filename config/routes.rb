d4c5ab3f048d365e96c0361171d2f286e7b3a960
Prima Aulia Gusta <primaulia@gmail.com>
2021-05-19 10:12:42 +0800

    ready to review

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :restaurants
-  devise_for :users
   root to: 'restaurants#index'
+  devise_for :users
+  resources :restaurants do
+    resources :reviews, only: [:create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
